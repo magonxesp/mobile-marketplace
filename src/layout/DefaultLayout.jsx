ed9c5efa9a77dbc3fdad408bf4c4c40103ee502f
MagonxESP <janma.360@gmail.com>
2025-07-19 14:01:04 +0200

    feat: setup tailwind css and default layout

---
+++ src/layout/DefaultLayout.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+export default function DefaultLayout({ children }) {
+    return (
+        <>
+            <Header />
+            <main>
+                {children}
+            </main>
+        </>
+    );
+}
